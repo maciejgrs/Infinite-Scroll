b9c3889dbde705fbaad57a18624ba580e4f26de8
Maciej Grs <maciejgrs@gmail.com>
2021-03-15 12:43:36 +0100

    fetching the data

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
- 
+ const url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=30`
 
-const url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=30`
+ const fetchData = async () => {
+     try {
+         const response = await fetch(url)
+         const data = await response.json()
+         console.log(data);
+     }
+     catch (err) {
+        console.log(err);
+     }
+ }
+
+ fetchData()
