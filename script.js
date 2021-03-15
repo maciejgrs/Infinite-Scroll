ec21593400198dcc6cd87ad7490e0686e9e0ee7d
Maciej Grs <maciejgrs@gmail.com>
2021-03-15 13:10:55 +0100

    set atribute func

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
- const url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=30`
- const imageContainer = document.querySelector('.image-container')
- let arr
+const url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=30`;
+const imageContainer = document.querySelector(".image-container");
+let arr;
 
- const fetchData = async () => {
-     try {
-         const response = await fetch(url)
-         arr = await response.json()
-         displayImages()
-         
-     }
-     catch (err) {
-        console.log(err);
-     }
- }
+const fetchData = async () => {
+  try {
+    const response = await fetch(url);
+    arr = await response.json();
+    displayImages();
+  } catch (err) {
+    console.log(err);
+  }
+};
 
- fetchData()
+fetchData();
 
- const displayImages = () => {
-    arr.forEach(object => {
-        const img = document.createElement('img')
-        img.setAttribute('src', object.urls.regular)
-        imageContainer.appendChild(img)
-    }
-        )
- }
+const setAttribute = (element, atr) => {
+  for (const key in atr) {
+    element.setAttribute(key, atr[key]);
+  }
+};
+ 
+const displayImages = () => {
+  arr.forEach((object) => {
+    const item = document.createElement("a");
+    setAttribute(item, {
+      href: object.links.html,
+      target: "_blank",
+    });
+    const img = document.createElement("img");
+    setAttribute(img, {
+      src: object.urls.regular,
+      alt: object.alt_description,
+      title: object.alt_description,
+    });
+    item.appendChild(img);
+    imageContainer.appendChild(item)
+  });
+};
  
